e95be59451bebf77fe61fcd23b98044d0dbacd6a
chrisriis <christoffer.riis@gmail.com>
2015-09-11 23:30:44 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Riiskit Child
+
+This is the child theme for Riiskit, a developer focused parent theme that's based on Underscore, HTML5 Boilerplate and SMACSS.
+
+Riiskit: https://github.com/Chrisriis/riiskit
+
+## Installation
+Install Riiskit first.
+
+Then you need to have installed both _npm_ and _gulp_. After that, you simply locate (cd) the folder with your CLI and write:
+
+* _npm install_
+* _gulp_
+
+Just wait a while for the packages to install and voila!
---
+++ functions.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Riiskit
+ *
+ * @since Riiskit Child 1.0.0
+ */
+
+/**
+ * Set the content's pixel-width based on the theme's design.
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = apply_filters( 'riiskit_child_content_width' , 1170 );
+}
+
+
+
+function riiskit_child_setup() {
+	//add_editor_style( array( 'inc/admin/css/editor-style.css' ) );
+
+	//add_image_size( '', 250, 100, false );
+}
+
+add_action( 'after_setup_theme', 'riiskit_child_setup', 20 );
+
+
+
+/**
+ * Enqueue development stylesheets for the front end.
+ *
+ * @since Riiskit Child 1.0.0
+ */
+function riiskit_child_dev_stylesheets() {
+    wp_enqueue_style( 'riiskit-child-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'riiskit-child-main', get_stylesheet_directory_uri() . '/dist/css/base/main.css', array(), '1.0.0', null );
+    wp_enqueue_style( 'riiskit-child-elements', get_stylesheet_directory_uri() . '/dist/css/base/elements.css', array(), '1.0.0', null );
+    wp_enqueue_style( 'riiskit-child-mobile-menu', get_stylesheet_directory_uri() . '/dist/css/components/mobile-menu.css', array(), '1.0.0', null );
+}
+
+/**
+ * Enqueue development scripts for the front end.
+ *
+ * @since Riiskit Child 1.0.0
+ */
+function riiskit_child_dev_scripts() {
+    wp_enqueue_script( 'riiskit-child-main', get_stylesheet_directory_uri() . '/src/js/main.js', array( 'jquery' ), '1.0.0' );
+    wp_enqueue_script( 'riiskit-child-mobile-menu', get_stylesheet_directory_uri() . '/src/js/components/mobile-menu.js', array( 'riiskit_child_main' ), '1.0.0' );
+}
+
+/**
+ * Enqueue production stylesheets for the front end.
+ *
+ * @since Riiskit Child 1.0.0
+ */
+function riiskit_child_dist_stylesheets() {
+    // Parent styles
+    wp_enqueue_style( 'riiskit-style', get_template_directory_uri() . '/style.css' );
+    // Child styles
+    wp_enqueue_style( 'riiskit-child-main', get_stylesheet_directory_uri() . '/dist/css/min/main.min.css', array(), '1.0.0', null );
+}
+
+/**
+ * Enqueue production scripts for the front end.
+ *
+ * @since Riiskit Child 1.0.0
+ */
+function riiskit_child_dist_scripts() {
+    wp_enqueue_script( 'riiskit-child-main', get_stylesheet_directory_uri() . '/dist/js/min/main.min.js', array( 'jquery' ), '1.0.0' );
+}
+
+/* Uncomment for production */
+//add_action('wp_enqueue_scripts', 'riiskit_child_dist_stylesheets');
+//add_action( 'wp_enqueue_scripts', 'riiskit_child_dist_scripts');
+/* Comment out for production */
+add_action( 'wp_enqueue_scripts', 'riiskit_child_dev_stylesheets' );
+add_action( 'wp_enqueue_scripts', 'riiskit_child_dev_scripts' );
+
+
+
+/**
+ * Inline JS and conditional JS (HTML5 shim, selectivizr).
+ *
+ * @since Riiskit 1.0.0
+ */
+function riiskit_child_scripts_head() { ?>
+	<!-- Battle the FOUC (remove if using Modernizr) -->
+	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js');})(document.documentElement);</script>
+<?php }
+add_action( 'wp_head', 'riiskit_child_scripts_head' );
+
+
+
+/**
+ * Plugin filters.
+ *
+ * @since Riiskit Child 1.0.0
+ */
+//require_once( RIISKIT_BASE . 'inc/plugin-filters.php' );
+
+/**
+ * Custom post types.
+ *
+ * @since Riiskit Child 1.0.0
+ */
+//require_once( RIISKIT_BASE . 'inc/admin/custom-post-types.php' );
+
+/**
+ * Custom logos etc.
+ *
+ * @since Riiskit Child 1.0.0
+ */
+//require_once( RIISKIT_BASE . 'inc/admin/customization.php' );
+
+/**
+ * Metaboxes.
+ *
+ * @since Riiskit Child 1.0.0
+ */
+//require_once( RIISKIT_BASE . 'inc/admin/metaboxes.php' );
+
+?>
