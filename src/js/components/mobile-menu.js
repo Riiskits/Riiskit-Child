f69a0b5012f47fcc53d4e782d9660d7a6fc40ab3
chrisriis <christoffer.riis@gmail.com>
2015-09-15 18:57:36 +0200

    Throttle resize and bugfixes.

--- src/js/components/mobile-menu.js
+++ src/js/components/mobile-menu.js
@@ -20,36 +20,40 @@ jQuery(function ($) {
 
     if ( !nav || !button ) {
         return;
-    }
-    if ( !menu || !menu.children().length ) {
+    } else if ( !menu || !menu.children().length ) {
         button.addClass('hide');
         return;
-    }
+    } else {
+        // Toggle functionality
+       button.on('click.riiskit', function() {
+            menu.toggleClass('active');
+            $(this).toggleClass('active');
 
-    // Toggle functionality
-   button.on('click.riiskit', function(){
-        menu.toggleClass('active');
-        $(this).toggleClass('active');
-
-        if ( active === false ) {
-            active = true;
-            $(this).attr('aria-pressed', 'true');
-        } else {
-	        active = false;
-	        $(this).attr('aria-pressed', 'false');
-        }
-    });
-
-    // Make the menu visible again if it's is closed while
-    // the window is resized past the breakpoint.
-    $.selector_cache(window).on('resize.riiskit', function(){
-        if ( $.selector_cache(window).width() >= menuBreakpoint ) {
-            menu.addClass('show');
-        } else {
-            if ( !menu.hasClass('active') ) {
-                menu.removeClass('show');
+            if ( active === false ) {
+                active = true;
+                $(this).attr('aria-pressed', 'true');
+            } else {
+    	        active = false;
+    	        $(this).attr('aria-pressed', 'false');
             }
-        }
-    });
+        });
+
+       // Resize window
+        var menuResize = riiskit.utils.throttle(function() {
+            console.log( 'test' );
+            if ( $.selector_cache(window).width() >= menuBreakpoint ) {
+                menu.addClass('active was-not-active');
+                button.addClass('active');
+            } else {
+                if ( menu.hasClass('active was-not-active') ) {
+                    menu.removeClass('active active was-not-active');
+                    button.removeClass('active active was-not-active');
+                }
+            }
+        });
+
+        $.selector_cache(window)
+            .on('resize.riiskit', menuResize);
+    }
 
 }); // end jQuery document ready
